0ab249e89eb06d6f9d268276890c4bcb8c12fad7
liuyan <vsofoliuyan@126.com>
2020-12-07 18:40:14 +0800

    完善

--- FastSocket.Server/obj/Debug/netstandard2.0/FastSocket.AssemblyInfo.cs
+++ FastSocket.Server/obj/Debug/netstandard2.0/FastSocket.AssemblyInfo.cs
@@ -11,8 +11,9 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("FastSocket")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("刘颜")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyDescriptionAttribute("Socket通信，服务端")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FastSocket")]
